ca1a930bcc3e5e8c13c76cba4e08b26fa497b5ac
Tyler O'Briant <tylerobriant@gmail.com>
2023-07-12 10:17:38 -0500

    challenge six working without a framework

--- challenge_6/main.go
+++ challenge_6/main.go
@@ -1,136 +1,68 @@
 package main
 
 import (
-	"bufio"
-	"cryptopals/cipher"
 	"cryptopals/decrypt"
 	"encoding/base64"
-	"encoding/hex"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"sort"
 	"strings"
 )
 
-const (
-	input = `I'm back and I'm ringin' the bell
-A rockin' on the mike while the fly girls yell
-In ecstasy in the back of me
-Well that's my DJ Deshay cuttin' all them Z's
-Hittin' hard and the girlies goin' crazy
-Vanilla's on the mike, man I'm not lazy.
-
-I'm lettin' my drug kick in
-It controls my mouth and I begin
-To just let it flow, let my concepts go
-My posse's to the side yellin', Go Vanilla Go!
-
-Smooth 'cause that's the way I will be
-And if you don't give a damn, then
-Why you starin' at me
-So get off 'cause I control the stage
-There's no dissin' allowed
-I'm in my own phase
-The girlies sa y they love me and that is ok
-And I can dance better than any kid n' play
-
-Stage 2 -- Yea the one ya' wanna listen to
-It's off my head so let the beat play through
-So I can funk it up and make it sound good
-1-2-3 Yo -- Knock on some wood
-For good luck, I like my rhymes atrocious
-Supercalafragilisticexpialidocious
-I'm an effect and that you can bet
-I can take a fly girl and make her wet.
-
-I'm like Samson -- Samson to Delilah
-There's no denyin', You can try to hang
-But you'll keep tryin' to get my style
-Over and over, practice makes perfect
-But not if you're a loafer.
-
-You'll get nowhere, no place, no time, no girls
-Soon -- Oh my God, homebody, you probably eat
-Spaghetti with a spoon! Come on and say it!
-
-VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino
-Intoxicating so you stagger like a wino
-So punks stop trying and girl stop cryin'
-Vanilla Ice is sellin' and you people are buyin'
-'Cause why the freaks are jockin' like Crazy Glue
-Movin' and groovin' trying to sing along
-All through the ghetto groovin' this here song
-Now you're amazed by the VIP posse.
-
-Steppin' so hard like a German Nazi
-Startled by the bases hittin' ground
-There's no trippin' on mine, I'm just gettin' down
-Sparkamatic, I'm hangin' tight like a fanatic
-You trapped me once and I thought that
-You might have it
-So step down and lend me your ear
-'89 in my time! You, '90 is my year.
-
-You're weakenin' fast, YO! and I can tell it
-Your body's gettin' hot, so, so I can smell it
-So don't be mad and don't be sad
-'Cause the lyrics belong to ICE, You can call me Dad
-You're pitchin' a fit, so step back and endure
-Let the witch doctor, Ice, do the dance to cure
-So come up close and don't be square
-You wanna battle me -- Anytime, anywhere
-
-You thought that I was weak, Boy, you're dead wrong
-So come on, everybody and sing this song
-
-Say -- Play that funky music Say, go white boy, go white boy go
-play that funky music Go white boy, go white boy, go
-Lay down and boogie and play that funky music till you die.
-
-Play that funky music Come on, Come on, let me hear
-Play that funky music white boy you say it, say it
-Play that funky music A little louder now
-Play that funky music, white boy Come on, Come on, Come on
-Play that funky music`
-)
+var charFreq = map[string]float64{
+	"a": 0.0651738, "b": 0.0124248, "c": 0.0217339, "d": 0.0349835, "e": 0.1041442, "f": 0.0197881, "g": 0.0158610,
+	"h": 0.0492888, "i": 0.0558094, "j": 0.0009033, "k": 0.0050529, "l": 0.0331490, "m": 0.0202124, "n": 0.0564513,
+	"o": 0.0596302, "p": 0.0137645, "q": 0.0008606, "r": 0.0497563, "s": 0.0515760, "t": 0.0729357, "u": 0.0225134,
+	"v": 0.0082903, "w": 0.0171272, "x": 0.0013692, "y": 0.0145984, "z": 0.0007836, " ": 0.1918182,
+}
 
 func main() {
-	b := readFile2()
+	src := readFile()
 
-	results := make([]decrypt.ApproximatorResult, 39)
+	keysize := approximateKeysize(src)
+
+	// Now we go through and group together the bytes that would be worked on by each part of the key into their own ciphers
+	ciphers := groupSingleBitXorCiphers(src, keysize)
+
+	// Single-Bit Xor Decrypt each individual cipher, then add the best scored result to []key
+	key := make([]byte, len(ciphers))
+	for i, cipher := range ciphers {
+		key[i] = decryptSingleBitXor(cipher)
+	}
+
+	decryptWithKeyAndPrint(src, key)
+}
+
+func approximateKeysize(src []byte) int {
+	approxResults := make([]decrypt.ApproximatorResult, 39)
 	for i := 2; i <= 40; i++ {
-		results[i-2] = decrypt.ApproximatorResult{
+		approxResults[i-2] = decrypt.ApproximatorResult{
 			Keysize:                i,
 			AverageHammingDistance: 0,
 		}
 	}
 
-	fmt.Println("len(b)", len(b))
 	// For each keysize
-	for i, r := range results {
+	for i, r := range approxResults {
 		distances := []float64{}
 
 		if r.Keysize == 0 {
-			fmt.Println("hey boss it's zero")
 			continue
 		}
 
 		// Calculate the Hamming Distance for each subset of pairs of size r.Keysize
-		for j := 0; j < len(b)-(len(b)%r.Keysize)-r.Keysize; j += r.Keysize {
-			// for j := 0; j < 29*3; j += r.Keysize {
+		for j := 0; j < len(src)-(len(src)%r.Keysize)-r.Keysize; j += r.Keysize {
 
 			// First block of size r.Keysize, block n
-			a := b[j : j+r.Keysize]
+			a := src[j : j+r.Keysize]
 
 			// Second following block of size r.Keysize, block n+1
-			b := b[j+r.Keysize : j+r.Keysize*2]
+			b := src[j+r.Keysize : j+r.Keysize*2]
 
 			normalizedDistance := float64(decrypt.HammingDistance(a, b)) / float64(r.Keysize)
 			distances = append(distances, normalizedDistance)
 		}
 
-		fmt.Println(r.Keysize, "\n\n", distances, "\n\n")
 		// Normalize distances by keysize, then add to running sum
 		var sum float64 = 0
 		for j := 0; j < len(distances); j++ {
@@ -138,165 +70,87 @@ func main() {
 		}
 
 		// Calculate the average
-		results[i].AverageHammingDistance = sum / float64(len(distances))
+		approxResults[i].AverageHammingDistance = sum / float64(len(distances))
 
 	}
 
-	// Sort results so that the shortest Hamming Distance is first
-	sort.Slice(results, func(i, j int) bool {
-		return results[i].AverageHammingDistance < results[j].AverageHammingDistance
+	// Sort approxResults so that the shortest Hamming Distance is first
+	sort.Slice(approxResults, func(i, j int) bool {
+		return approxResults[i].AverageHammingDistance < approxResults[j].AverageHammingDistance
 	})
 
-	for _, r := range results {
-		fmt.Println("key: ", r.Keysize, "d: ", r.AverageHammingDistance)
-	}
-
-	keysize := results[0].Keysize
-
-	// Crack it
-	// So I need to break everything up into sections of size keysize
-	fullIterations := len(b) / keysize
-	fmt.Println("keysize: ", keysize)
-	fmt.Println("fi: ", fullIterations)
-	singleBitCiphers := make([][]byte, keysize)
-	for i := range singleBitCiphers {
-		singleBitCiphers[i] = []byte{}
-	}
-
-	// full iterations
-	for i := 0; i < fullIterations; i++ {
-		cipherIndex := i % keysize
-		// cipherLen := len(singleBitCiphers[cipherIndex])
-
-		singleBitCiphers[cipherIndex] = append(singleBitCiphers[cipherIndex], b[i])
-	}
-
-	decrypters := make([]decrypt.SingleBitXorDecrypter, keysize)
-	bestResults := make([]decrypt.Result, keysize)
-	for i, sbc := range singleBitCiphers {
-		fmt.Println(sbc)
-
-		decrypters[i] = decrypt.NewSingleBitXorDecrypter(
-			cipher.NewCipher(
-				hex.EncodeToString(sbc),
-			),
-		)
-
-		decrypters[i].Decrypt()
-
-		bestResults[i] = decrypters[i].Results[0]
-	}
-
-	for _, result := range bestResults {
-		fmt.Println(result.Key, result.DecryptedText)
-	}
-
+	return approxResults[0].Keysize
 }
 
-// func main2() {
-// 	cipher := readFile2()
-// 	// cipher := readFile()
-// 	// fmt.Println(cipher.Plaintext)
-// 	ka := decrypt.NewKeysizeApproximator(cipher)
-// 	ka.Evaluate()
-// 	for _, r := range ka.Results {
-// 		fmt.Println("key: ", r.Keysize, "d: ", r.AverageHammingDistance)
-// 	}
-
-// 	key := []byte{0x54, 0x65, 0x72, 0x6d, 0x69, 0x6e, 0x61, 0x74, 0x6f, 0x72, 0x20, 0x58, 0x3a, 0x20, 0x42, 0x72, 0x69, 0x6e, 0x67, 0x20, 0x74, 0x68, 0x65, 0x20, 0x6e, 0x6f, 0x69, 0x73, 0x65}
-// 	key2 := []byte{84, 101, 114, 109, 105, 110, 97, 116, 111, 114, 32, 88, 58, 32, 66, 114, 105, 110, 103, 32, 116, 104, 101, 32, 110, 111, 105, 115, 101}
-// 	fmt.Println(key)
-// 	fmt.Println(string(key))
-// 	fmt.Println(string(key2))
-
-// 	e := encrypt.NewRepeatingKeyXorEncrypter(input, string(key))
-// 	fmt.Println(e.Cipher.Plaintext[0:50])
-// 	fmt.Println(cipher.Plaintext[0:50])
-// 	fmt.Println(e.Cipher.Hex[0:50])
-// 	fmt.Println(cipher.Hex[0:50])
-// 	fmt.Println(e.Cipher.Binary[0:50])
-// 	fmt.Println(cipher.Binary[0:50])
-// 	fmt.Println(e.Cipher.Plaintext[:66] == cipher.Plaintext[:66])
-
-// 	fmt.Println(len(cipher.Plaintext) == len(e.Cipher.Plaintext), len(cipher.Plaintext), len(e.Cipher.Plaintext))
-
-// 	for i := 0; i < len(e.Cipher.Binary); i++ {
-// 		j := i % len(key)
-// 		fmt.Print(string(e.Cipher.Binary[i] ^ key[j]))
-// 	}
-
-// 	for i := 0; i < len(cipher.Binary); i++ {
-// 		j := i % len(key)
-// 		fmt.Print(string(cipher.Binary[i] ^ key[j]))
-// 	}
-
-// 	// fmt.Println(string(cipher.Plaintext[0:70]))
-
-// 	// fmt.Println(string(e.Cipher.Plaintext[0:70]))
-// }
-
-func readFile() cipher.Cipher {
-	file, _ := os.Open("./challenge_6/data.txt")
-	defer file.Close()
-
-	var lines []string
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		// fmt.Println(line, "\n")
-		lines = append(lines, line)
+func groupSingleBitXorCiphers(src []byte, keysize int) [][]byte {
+	ciphers := make([][]byte, keysize)
+	for i := range ciphers {
+		ciphers[i] = []byte{}
 	}
 
-	src := strings.Join(lines, "")
+	for i := 0; i < len(src); i++ {
+		cipherIndex := i % keysize
+		ciphers[cipherIndex] = append(ciphers[cipherIndex], src[i])
+	}
 
-	// fmt.Println(src)
+	return ciphers
+}
 
-	b, _ := base64.StdEncoding.DecodeString(src)
+func decryptSingleBitXor(src []byte) byte {
+	l := len(src)
 
-	// fmt.Println(b)
-	// fmt.Println(string(b))
+	type ScoringResult struct {
+		Score float64
+		Key   byte
+	}
 
-	// // Decode the base64-encoded hex string
-	// fmt.Println(string(data))
-	// h, err := base64.StdEncoding.decode
-	// if err != nil {
-	// 	fmt.Println("Error decoding base64:", err)
-	// }
+	scoringResults := make([]ScoringResult, 256)
+	for i := 0; i < 256; i++ {
 
-	return cipher.NewCipher(string(b))
+		dst := make([]byte, l)
+		for j := 0; j < len(src); j++ {
+			dst[j] = src[j] ^ byte(i)
+		}
+
+		str := string(dst)
+		score := 0.0
+
+		lowercase := strings.ToLower(str)
+		for i := 0; i < len(str); i++ {
+			char := string(lowercase[i])
+			val, ok := charFreq[char]
+
+			if !ok {
+				continue
+			}
+
+			score += val
+		}
+
+		scoringResults[i] = ScoringResult{
+			Key:   byte(i),
+			Score: score,
+		}
+	}
+
+	sort.Slice(scoringResults, func(i, j int) bool {
+		return scoringResults[i].Score > scoringResults[j].Score
+	})
+
+	return scoringResults[0].Key
+}
+
+func decryptWithKeyAndPrint(b []byte, k []byte) {
+	for i, v := range b {
+		fmt.Print(string(v ^ k[i%len(k)]))
+	}
 }
 
 // Maybe my assumption is just bad. I've been expecting the decoded base64 to be
-func readFile2() []byte {
+func readFile() []byte {
 	data, _ := ioutil.ReadFile("./challenge_6/data.txt")
 
-	decodedBytes, _ := base64.StdEncoding.DecodeString(string(data))
-	fmt.Println(decodedBytes)
-	fmt.Println(string(decodedBytes))
+	b, _ := base64.StdEncoding.DecodeString(string(data))
 
-	key := []byte{0x54, 0x65, 0x72, 0x6d, 0x69, 0x6e, 0x61, 0x74, 0x6f, 0x72, 0x20, 0x58, 0x3a, 0x20, 0x42, 0x72, 0x69, 0x6e, 0x67, 0x20, 0x74, 0x68, 0x65, 0x20, 0x6e, 0x6f, 0x69, 0x73, 0x65}
-
-	for i := 0; i < len(decodedBytes); i++ {
-		j := i % len(key)
-		fmt.Print(string(decodedBytes[i] ^ key[j]))
-	}
-
-	c := cipher.NewCipher(string(decodedBytes))
-
-	fmt.Println("\n\n-----------\n\n")
-
-	for i := 0; i < len(c.Binary); i++ {
-		j := i % len(key)
-		fmt.Print(string(c.Binary[i] ^ key[j]))
-	}
-
-	// c2 := readFile()
-
-	// fmt.Println("binary equal?", reflect.DeepEqual(c2.Binary, decodedBytes))
-	// fmt.Println("hex equal?", reflect.DeepEqual(c2.Hex, c.Hex))
-	// fmt.Println("plaintext equal?", reflect.DeepEqual(c2.Plaintext, c.Plaintext))
-	// return c
-
-	return decodedBytes
+	return b
 }
